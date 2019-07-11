2cdc492bca741923e52376b63102ce582c938b87
Pieter Joost <pvandesande@deloitte.nl>
2019-07-11 22:10:39 +0200

    switch to slack

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -58,7 +58,7 @@ class Footer extends React.Component {
               rel="noreferrer noopener">
               Stack Overflow
             </a>
-            <a href="https://discordapp.com/">Project Chat</a>
+            <a href="/chat">Project Chat</a>
             <a
               href="https://twitter.com/streamsdb"
               target="_blank"
