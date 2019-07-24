5150ba1d034c3bd39354054ed964264e379c1489
Pieter Joost van de Sande <pj@born2code.net>
2019-07-24 12:08:33 +0200

    fix getting-started link in footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,7 +37,7 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl('getting-started', this.props.language)}>
+            <a href={this.docUrl('getting-started-cli', this.props.language)}>
               Getting Started
             </a>
             <a href={this.docUrl('core-concepts', this.props.language)}>
